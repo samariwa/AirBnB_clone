b4c0254c39bb0b91ec0c1f323a170e439082649f
Bezawork-pr <bezaworkalx@gmail.com>
2022-11-25 03:08:35 -0800

    Fix show function to show str print of instance

--- console.py
+++ console.py
@@ -8,7 +8,7 @@ The command interpretor should implement
 quit and EOF to exit the program
 This class deals with providing custom prompt
 
-create: 
+create:
     Creates a new instance of BaseModel
     saves it (to the JSON file)
     prints the id
@@ -18,6 +18,7 @@ create:
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from datetime import datetime
 
 
 class HBNBCommand(cmd.Cmd):
@@ -54,11 +55,30 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         if arg != "":
-            class_name, class_id = arg.split(" ")
-            if class_name == "BaseModel":
+            try:
+                class_name, class_id = arg.split(" ")
+            except ValueError:
+                if arg == "BaseModel":
+                    print("** instance id missing **")
+                else:
+                    print("** class doesn't exist **")
+                return
+            if class_name == "BaseModel" and class_id != "":
                 my_dict = storage.all()
                 my_id = "BaseModel" + "." + class_id
-                print(my_dict[my_id])
+                try:
+                    d = my_dict[my_id]
+                except:
+                    print("** no instance found **")
+                    return
+                my_object = "[BaseModel] ({:s}) {}"
+                f = "%Y-%m-%dT%H:%M:%S.%f"
+                d['created_at'] = datetime.strptime(d['created_at'], f)
+                d['updated_at'] = datetime.strptime(d['updated_at'], f)
+                my_object = my_object.format(class_id, d)
+                print(my_object)
+        else:
+            print("** class name missing **")
 
     def do_destroy(self, arg):
         pass
@@ -68,6 +88,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
