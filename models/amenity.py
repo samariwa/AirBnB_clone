b39d2dbd2df0d19b0e279c92bd76b9672b394a1b
samariwa <samuelmariwa@gmail.com>
2022-11-26 05:36:20 +0000

    State and city classes implementation

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
-This module contains a class User which inherits from the BaseModel
+This module contains a class Amenity which inherits from the BaseModel
 Public class attributes:
-    email: string - empty string
-    password: string - empty string
-    first_name: string - empty string
-    last_name: string - empty string
+    name: string - empty string
 """
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
+class Amenity(BaseModel):
     """
-    This is the user class that inherits from the BaseModel class
+    This is the amenity class that inherits from the BaseModel class
     """
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    name = ''
